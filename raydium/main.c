6f937662f8ed9e372cc7591f9dd06286c21975e5
Julien Xfennec <xfennec@cqfd-corp.org>
2005-09-21 18:54:01 +0000

    version changed to 0.680 for new public release

--- raydium/main.c
+++ raydium/main.c
@@ -17,7 +17,7 @@
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-char *raydium_version="0.644";
+char *raydium_version="0.680";
 
 #define MAIN_C
 #include "common.h"
