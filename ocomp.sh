6e38e2a96b44c9b5533b82e8085b51459ce411cf
Julien Xfennec <xfennec@cqfd-corp.org>
2006-03-19 16:01:35 +0000

    *comp.sh scripts now correctly pass all arguments to application

--- ocomp.sh
+++ ocomp.sh
@@ -7,4 +7,5 @@ ulimit -c 0
 rm test
 gcc -g $1 -Wall -o test -L/usr/X11R6/lib/ -lXinerama -lGL -lGLU -lm -lopenal -lalut -ljpeg -Iode/include/ ode/lib/libode.a -lvorbis -lvorbisfile -logg -Iphp/ -Iphp/include -Iphp/main/ -Iphp/Zend -Iphp/TSRM php/libs/libphp4.a -lresolv -lcrypt -lz
 sync
-./test $2 $3 $4 $5 $6
+shift
+./test "$@"
--- odyncomp.sh
+++ odyncomp.sh
@@ -15,4 +15,5 @@ rm test
 gcc $1 -g -Wall -DFORCE_LIBRAYDIUM -o test libraydium.so -Iphp/ -Iphp/main/ -Iphp/Zend -Iphp/TSRM -Iode/include/
 sync
 export LD_LIBRARY_PATH=.
-./test $2 $3 $4 $5 $6
+shift
+./test "$@"
