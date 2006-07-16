fc25d51569dab304b7792c961857eb74a2352b73
Julien Xfennec <xfennec@cqfd-corp.org>
2006-07-16 12:41:51 +0000

    Small change to follow OpenAL spec: switching to NULL context before deleting the current context

--- raydium/sound.c
+++ raydium/sound.c
@@ -623,17 +623,18 @@ ALCcontext* pContext;
    alDeleteBuffers(RAYDIUM_SOUND_NUM_BUFFERS,raydium_sound_buffer);
   raydium_log("sound: Releasing OpenAL");
    
-/* Strange ... alutExit seems to do the job under Win32.
+
 #ifdef WIN32
   	//Get active context
 	pContext=alcGetCurrentContext();
 	//Get device for active context
 	pDevice=alcGetContextsDevice(pContext);
 	//Release context(s)
-	alcDestroyContext(pContext);
+    alcMakeContextCurrent(NULL);
+    alcDestroyContext(pContext);
 	//Close device
 	alcCloseDevice(pDevice);
-#endif*/
+#endif
   alutExit();
  }
 }
