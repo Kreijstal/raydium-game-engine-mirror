48dc8724a6d486c0c0345d277810168c3133103b
Julien Xfennec <xfennec@cqfd-corp.org>
2006-11-13 22:06:58 +0000

    tangent space vectors smoothing is now available

--- raydium/headers/normal.h
+++ raydium/headers/normal.h
@@ -9,9 +9,9 @@ Normals
 // Introduction
 /**
 This file provides some usefull functions for normal generation and smoothing.
-You can find some more informations about normals at the top of this guide. 
+You can find some more informations about normals at the top of this guide.
+It now provide a few functions about tangent vectors smoothing.
 **/
-
 
 __rayapi void raydium_normal_generate_lastest_triangle (int default_visu);
 /**
@@ -37,16 +37,31 @@ This function will regenerate standard and visu normals for the whole
 scene (ground, objects, ...).
 **/
 
+__rayapi void raydium_normal_internal_smooth_generic(char *type,GLuint from, GLuint to,GLfloat *in_x, GLfloat *in_y, GLfloat *in_z,GLfloat *out_x, GLfloat *out_y, GLfloat *out_z);
+/**
+Internal. Generic smoothing function.
+**/
+
 __rayapi void raydium_normal_smooth_all (void);
 /**
 This function will smooth the whole scene, using adjacent vertices.
 Note this function can take a lot of time.
 **/
 
-void raydium_normal_smooth_from_to(GLuint from, GLuint to);
+__rayapi void raydium_normal_smooth_from_to(GLuint from, GLuint to);
 /**
 Same as above, but only from ##from## vertex to ##to## vertex (excluded).
 In other words: will smooth [from;to[
+**/
+
+__rayapi void raydium_normal_tangent_smooth_all (void);
+/**
+Same as ##raydium_normal_smooth_all()##, but for tangent vectors.
+**/
+
+__rayapi void raydium_normal_smooth_tangent_from_to(GLuint from, GLuint to);
+/**
+Same as ##raydium_normal_smooth_from_to()##, but for tangent vectors.
 **/
 
 
