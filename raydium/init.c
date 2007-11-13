aeefb0deb34a907dedab703aac9bbb2bfe0dcba3
Vicente Carro F <vicentecarro@gmail.com>
2007-11-13 00:52:33 +0000

    Added default fail-safe initial configuration when the file is missing in function raydium_init_load().

--- raydium/init.c
+++ raydium/init.c
@@ -458,6 +458,21 @@ int raydium_init_load(char *filename)
     {
         //we can not load the configuration. ERROR
         raydium_log("ERROR loading configuration file.");
+        raydium_log("Loading a default fail-safe configuration.");
+        //raydium_init_args(argc,argv);
+        raydium_window_create(640,480,RAYDIUM_RENDERING_WINDOW,"My app");
+
+        raydium_texture_filter_change(RAYDIUM_TEXTURE_FILTER_TRILINEAR);
+        raydium_window_view_perspective(60,0.01,2500); // fov 60 + near and far planes
+
+        raydium_fog_disable();    
+        raydium_light_enable();
+        raydium_light_on(0);
+
+        raydium_light_conf_7f(0,50,150,200,1000000,1,0.9,0.7); // id, pos, intensity and color (RGB)
+        raydium_background_color_change(1,0.9,0.7,1);
+
+        raydium_sky_box_cache();
         return 0;
     }
 }
