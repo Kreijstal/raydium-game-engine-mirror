68ab7f5c11a0bb8d24ae8b847bd83348b5817563
Julien Xfennec <xfennec@cqfd-corp.org>
2008-08-18 17:39:50 +0000

    Added OCOMP_FLAGS env var support to ocomp.sh. Useful for GNU profiler: export OCOMP_FLAGS="-pg"

--- ocomp.sh
+++ ocomp.sh
@@ -8,7 +8,7 @@ rm test
 gcc -g "$1" -Wall -o test -L/usr/X11R6/lib/ -lXinerama -lGL -lGLU -lm -lopenal -lalut -ljpeg \
 -Iraydium/ode/include/ raydium/ode/ode/src/libode.a -lvorbis -lvorbisfile -logg \
 -Iraydium/php/ -Iraydium/php/include -Iraydium/php/main/ -Iraydium/php/Zend -Iraydium/php/TSRM raydium/php/libs/libphp5.a \
--lresolv -lcrypt -lz -lcurl -lxml2 -lGLEW
+-lresolv -lcrypt -lz -lcurl -lxml2 -lGLEW $OCOMP_FLAGS
 sync
 shift
 ./test "$@"
