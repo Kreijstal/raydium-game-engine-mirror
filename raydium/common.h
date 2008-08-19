58afa24fc1528dba5ec9dbac00cebf9ce53e320f
Julien Xfennec <xfennec@cqfd-corp.org>
2008-08-19 21:07:31 +0000

    Version is now 0.800 ! Added another fixfor new licenses

--- raydium/common.h
+++ raydium/common.h
@@ -1,26 +1,14 @@
 #ifndef _CORE_COMMON_H
 #define _CORE_COMMON_H
 /*
-* Raydium - CQFD Corp.
-* http://raydium.org/
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+    Raydium - CQFD Corp.
+    http://raydium.org/
+    Released under both BSD license and Lesser GPL library license.
+    See "license.txt" file.
 */
 
 #define RAYDIUM_MAJOR   0
-#define RAYDIUM_MINOR   706
+#define RAYDIUM_MINOR   800
 
 #ifdef WIN32
 # ifdef RAYDLL
