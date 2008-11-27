98fbad86715b5d72b42de312a0cadcc7c4046d3f
ouille <ouille@gmail.com>
2008-11-27 17:31:23 +0000

    Raydium console now report Ode version.

--- raydium/ode.c
+++ raydium/ode.c
@@ -213,7 +213,7 @@ dWorldSetContactMaxCorrectingVel(raydium_ode_world,10);
 // wow ! This is my first init dependance ;)
 raydium_ode_object_create("GLOBAL");
 raydium_ode_network_init();
-
+raydium_log("physics: ODE (%s)",VERSION);
 raydium_log("physics: ODE Net: %i element(s)/packet",raydium_ode_network_MaxElementsPerPacket());
 raydium_log("physics: OK");
 }
